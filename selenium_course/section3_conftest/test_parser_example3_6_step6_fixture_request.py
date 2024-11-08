6c6fcedd402ce623eed179901db72a1a3b6d6d3f
DmKolfild <dmitriybaranov0@gmail.com>
2024-11-08 22:20:28 +0300

    Added time for test

--- selenium_course/section3_conftest/test_parser_example3_6_step6_fixture_request.py
+++ selenium_course/section3_conftest/test_parser_example3_6_step6_fixture_request.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 
 
@@ -9,4 +10,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
+    time.sleep(5)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
