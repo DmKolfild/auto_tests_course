bf00e0d564a044173204a43dd7bad305f96d5c4a
DmKolfild <dmitriybaranov0@gmail.com>
2024-11-08 22:22:16 +0300

    Added comment how to run test

--- selenium_course/section3_conftest/test_lesson3_6_step10_language.py
+++ selenium_course/section3_conftest/test_lesson3_6_step10_language.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
+# pytest --language=es .\test_lesson3_6_step10_language.py
+
+
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
