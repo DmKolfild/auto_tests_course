b7911b0709104278873fb7813055ce020b1d53ed
DmKolfild <dmitriybaranov0@gmail.com>
2024-11-10 11:49:39 +0300

    Added locators.py

--- selenium_course/section4/conftest.py
+++ selenium_course/section4/conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-
 # pytest -s -v --browser_name=chrome --language=es file_name
 
 def pytest_addoption(parser):
