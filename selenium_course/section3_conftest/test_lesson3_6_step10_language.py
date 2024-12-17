cafb5ff617ee7e775a3616db196abc4ce37c982e
dnkolfild <dmitriy-b5@yandex.ru>
2024-12-17 13:05:10 +0300

    Fixed bug

--- selenium_course/section3_conftest/test_lesson3_6_step10_language.py
+++ selenium_course/section3_conftest/test_lesson3_6_step10_language.py
@@ -11,6 +11,6 @@ def test_supplys_contains_add_to_cart_button(browser):
     browser.implicitly_wait(10)
     browser.get(link)
 
-    button = browser.find_element(By.CSS_SELECTOR, "#id_quantity + button")
+    button = browser.find_elements(By.CSS_SELECTOR, "#id_quantity + button")
 
     assert button, "Button is not found!"
